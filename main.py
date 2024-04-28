0afdc8d3fd3806dac97f286c905f260585ba2588
Alf <alfred.s.sandare04@gmail.com>
2024-04-28 14:36:04 +0200

    added load from menues.json and interpretation functionality

--- main.py
+++ main.py
@@ -9,9 +9,22 @@ class Game:
         self.screen = pygame.display.set_mode((800, 800))
         self.menu_handler = MenuHandler(self.screen, 1)
 
+        menues_data = self.load_menues_data()
+        self.menu_handler.load_data_from_dict(menues_data, None)
+
+    def load_menues_data(self):
         with open(PATH + "data/menues.json") as file:
             data = json.load(file)
-            print(data)
+            data = json.dumps(data)  # make it a string
+            values = data.split('"@get ')
+
+            final_data = values.pop(0)
+            for value in values:
+                split = value.split('@"')
+                final_data += (str(self.get_values(split[0])) 
+                               + ("" if len(split) == 1 else split[1]))
+
+        return json.loads(final_data)
 
     def main(self):
         clock = pygame.time.Clock()
@@ -23,6 +36,14 @@ class Game:
             pygame.display.flip()
             clock.tick(60)
 
+    def get_values(self, input_):
+        id_ = input_.split()[0]
+        args = input_.split()[1:]
+
+        if id_ == "frame_size":
+            return [100, 100]
+        
+        return ""
 
 if __name__ == "__main__":
     game = Game()
