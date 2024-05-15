c2f3d89f5888865c87b54d50ddc5e576ca038253
Alfred Sändare Svensson <alfred.s.svensson@hogia.se>
2024-05-15 10:05:15 +0200

    added get_surface function

--- graphics/celestial_body_visual.py
+++ graphics/celestial_body_visual.py
@@ -29,28 +29,19 @@ class CelestialBodyVisual:
         self.rotation_progress = 0  # 0 - 1
         self.cloud_rotation_progress = 0  # 0 - 1
         
-
-    def draw(self, screen: pygame.Surface, pos: tuple[int, int], size: int):
+    def get_surface(self, size: int, rotation_progress: float = 0, cloud_rotation_progress: float = 0):
 
         diameter = self.surface_size[1]
         
         surface = pygame.Surface((diameter, diameter), pygame.SRCALPHA)
 
-        self.rotation_progress += self.surface_speed
-        if self.rotation_progress > 1:
-            self.rotation_progress -= 1
-
         surface.blit(self.planet_surface, (0, 0), 
-                    (self.rotation_progress * self.surface_size[0], 0, 
+                    (rotation_progress * self.surface_size[0], 0, 
                      self.surface_size[1], self.surface_size[1]))
         
         if self.cloud_surface is not None:
-            self.cloud_rotation_progress += self.cloud_speed
-            if self.cloud_rotation_progress > 1:
-                self.cloud_rotation_progress -= 1
-
             surface.blit(self.cloud_surface, (0, 0), 
-                         (self.cloud_rotation_progress * self.surface_size[0], 0, 
+                         (cloud_rotation_progress * self.surface_size[0], 0, 
                          self.surface_size[1], self.surface_size[1]))
         
         # Masks the part of the surface that is not in the circle
@@ -63,6 +54,21 @@ class CelestialBodyVisual:
 
         #mask = self.create_alpha_gradient(surface)
         #surface.blit(mask, (0, 0), special_flags=pygame.BLEND_RGBA_MULT)
+
+        return surface
+
+    def draw(self, screen: pygame.Surface, pos: tuple[int, int], size: int):
+        self.rotation_progress += self.surface_speed
+        if self.rotation_progress > 1:
+            self.rotation_progress -= 1
+
+        if self.cloud_surface is not None:
+            self.cloud_rotation_progress += self.cloud_speed
+            if self.cloud_rotation_progress > 1:
+                self.cloud_rotation_progress -= 1
+
+        surface = self.get_surface(size, self.rotation_progress, 
+                                   self.cloud_rotation_progress)
 
         pos = [pos[0]-size/2, pos[1]-size/2]
         screen.blit(surface, pos)
