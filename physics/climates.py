8143782adfb933c4b702f71e5072f45efa3e65eb
alfredsandare <alfred.s.sandare04@gmail.com>
2024-05-21 09:56:27 +0200

    added toxic parameter

--- physics/climates.py
+++ physics/climates.py
@@ -1,10 +1,14 @@
+import math
+
+
 class Climate:
-    def __init__(self, name: str, is_fluid: bool, temperature: float):
+    def __init__(self, name: str, is_fluid: bool, temperature: float, toxic: bool):
         self.name = name
 
         # These factors are only used for calculating cost of changes a district's climate.
         self.is_fluid = is_fluid  # whether or not this district is covered in a fluid (such as water or methane).
         self.temperature = temperature  # scale between 0 and 1.
+        self.toxic = toxic  # whether or not this district is toxic to life
 
     def get_switch_cost(self, to):
         cost = 10
@@ -12,12 +16,17 @@ class Climate:
         if self.is_fluid != to.is_fluid:
             cost += 5
 
-        cost += 5 * (self.temperature - to.temperature)
+        cost += 5 * math.abs(self.temperature - to.temperature)
+
+        if self.toxic != to.toxic:
+            cost += 5
+
+        return cost
 
 
-WATER_OCEAN = Climate("Water Ocean", True, 0.3)
-RAINFOREST = Climate("Rainforest", False, 0.35)
-ARID = Climate("Arid", False, 0.4)
-DESERT = Climate("Desert", False, 0.5)
-COLD_DESERT = Climate("Cold Desert", False, 0.2)
-TOXIC = Climate("Toxic", False, 0.6)
+WATER_OCEAN = Climate("Water Ocean", True, 0.3, False)
+RAINFOREST = Climate("Rainforest", False, 0.35, False)
+ARID = Climate("Arid", False, 0.4, False)
+DESERT = Climate("Desert", False, 0.5, False)
+COLD_DESERT = Climate("Cold Desert", False, 0.2, True)
+TOXIC = Climate("Toxic", False, 0.6, True)
