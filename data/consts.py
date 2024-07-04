2e7f1f7ce300c271e156fb0bdb9398407b012dab
Alf <alfred.s.sandare04@gmail.com>
2024-07-04 10:58:56 +0200

    fixed typo

--- data/consts.py
+++ data/consts.py
@@ -1,7 +1,7 @@
 METERS_PER_AU = 149_597_870_700
 
 # One year = 365.25 days.
-# One week = 365.35 / 52 = 7.02404 days.
+# One week = 365.25 / 52 = 7.02404 days.
 # Seconds in a week = 7.02404 * 86_400 
 SECONDS_PER_WEEK = 606_877
 
