40d0f1d7cfcdb09d53075e50c7134deb72ba2dac
Alf <alfred.s.sandare04@gmail.com>
2024-11-15 17:40:48 +0100

    added average habitatability functions

--- society/civ.py
+++ society/civ.py
@@ -57,3 +57,41 @@ class Civ:
 
     def time_tick(self):
         self.modifiers_handler.calculate_modifiers()
+
+    def get_average_species_tb_habitability(self, star_system_id: str,
+                                            tb_id: str, species_id: str):
+        # Returns the average habitability of a species on a terrestrial body.
+        # This is the average of all the districts' habitability where this species is present.
+
+        tb: TerrestrialBody = self.star_systems[star_system_id].get_all_cbs_dict()[tb_id]
+        habitability_sum = 0
+        count = 0
+        for i, sub_population in enumerate(tb.population.sub_populations):
+            if species_id in sub_population.get_species_ids():
+
+                modifier_id = f"species_district_habitability@"\
+                    +f"{tb.star_system_id}@{tb.id}@{i}@{species_id}"
+
+                habitability_sum += self.modifiers_handler.get_modifier(\
+                    modifier_id).get_value()
+
+                count += 1
+
+        return habitability_sum / count if count > 0 else 0
+
+    def get_total_average_species_tb_habitability(self, star_system_id: str,
+                                                  tb_id: str):
+        # Returns the average habitability of all species on a terrestrial body.
+        # This accounts for the population of each species,
+        # so a species with a larger population will have a larger impact on the average.
+
+        tb: TerrestrialBody = self.star_systems[star_system_id].get_all_cbs_dict()[tb_id]
+        present_species_ids = tb.population.get_species_ids()
+        print(present_species_ids)
+        print(tb.population.get_species_population("human"))
+        averages = [self.get_average_species_tb_habitability(star_system_id,
+                                                             tb_id, species_id)
+                    * tb.population.get_species_population(species_id)
+                    for species_id in present_species_ids]
+
+        return sum(averages) / tb.population.get_total_population()
