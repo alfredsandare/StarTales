3440bee02b5e6e42094f3353c8cb24bd197a8747
alfredsandare <alfred.s.sandare04@gmail.com>
2025-04-26 16:40:06 +0200

    added stellar lum and temperature consts #2.1

--- data/consts.py
+++ data/consts.py
@@ -46,3 +46,29 @@ RESOURCE_NAMES = {
 TEXT_COLOR_ORANGE = "255 150 0"
 TEXT_COLOR_GREEN = "0 255 0"
 TEXT_COLOR_RED = "255 0 0"
+
+# In units of Sun luminosity
+STELLAR_LUM_LIMITS = {
+    "O": (30000, 1000000),
+    "B": (25, 30000),
+    "A": (5, 25),
+    "F": (1.1, 5),
+    "G": (0.6, 1.1),
+    "K": (0.08, 0.6),
+    "M": (0, 0.08)
+}
+
+SUN_LUMINOSITY = 3.828 * 10**26
+
+# Keys are lower limits.
+TEMPERATURES_ADJECTIVES = {
+    150: "Extremely hot",
+    65: "Scorching",
+    50: "Warm",
+    41: "Lukewarm",
+    35: "Moderate",
+    31: "Chilly",
+    25: "Cold",
+    20: "Freezing",
+    0: "Cryogenic"
+}
